3e7d5b1ff57dac15f699c8a5b1cc66e02dbfda0f
carlos200514 <cfrojas05@gmail.com>
2026-05-21 12:09:12 -0600

    fix main

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,10 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
-// create datasource
 const dataSource = new ProductData("tents");
 
-// find the element where products will render
 const listElement = document.querySelector(".product-list");
 
-// create ProductList instance
 const productList = new ProductList("tents", dataSource, listElement);
 
-// initialize and render products
 productList.init();
