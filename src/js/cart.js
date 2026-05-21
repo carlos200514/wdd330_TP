68a0c5aef7b3ac1130c03bc89e569fd684303199
carlos200514 <cfrojas05@gmail.com>
2026-05-21 15:02:55 -0600

    add d button

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,28 +1,69 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage("so-cart") || [];
+
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+  addRemoveButtons();
 }
 
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+  const newItem = `
+    <li class="cart-card divider">
+      <a href="#" class="cart-card__image">
+        <img
+          src="${item.Image}"
+          alt="${item.Name}"
+        />
+      </a>
+
+      <a href="#">
+        <h2 class="card__name">${item.Name}</h2>
+      </a>
+
+      <p class="cart-card__color">
+        ${item.Colors[0].ColorName}
+      </p>
+
+      <p class="cart-card__quantity">qty: 1</p>
+
+      <p class="cart-card__price">
+        $${item.FinalPrice}
+      </p>
+
+      <button
+        class="remove-item"
+        data-id="${item.Id}"
+      >
+        Remove
+      </button>
+    </li>
+  `;
 
   return newItem;
+}
+
+function addRemoveButtons() {
+  const buttons = document.querySelectorAll(".remove-item");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", removeItem);
+  });
+}
+
+function removeItem(event) {
+  const productId = event.target.dataset.id;
+
+  let cartItems = getLocalStorage("so-cart") || [];
+
+  cartItems = cartItems.filter((item) => item.Id !== productId);
+
+  setLocalStorage("so-cart", cartItems);
+
+  renderCartContents();
 }
 
 renderCartContents();
