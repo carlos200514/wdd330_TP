c7df8af345a25a8b78a93782f989d5a8df996440
carlos200514 <cfrojas05@gmail.com>
2026-05-21 11:40:09 -0600

    team w2

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,68 @@
+import { setLocalStorage, getLocalStorage } from './utils.mjs';
+
+export default class ProductDetails {
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+  }
+
+  async init() {
+    // get product data
+    this.product = await this.dataSource.findProductById(this.productId);
+
+    // render HTML
+    this.renderProductDetails();
+
+    // add cart listener
+    document
+      .getElementById('addToCart')
+      .addEventListener('click', this.addProductToCart.bind(this));
+  }
+
+  addProductToCart() {
+    let cart = getLocalStorage('so-cart') || [];
+
+    cart.push(this.product);
+
+    setLocalStorage('so-cart', cart);
+  }
+
+  renderProductDetails() {
+    const product = this.product;
+
+    document.querySelector('.product-detail').innerHTML = `
+      <h3>${product.Brand.Name}</h3>
+      <h2 class="divider">${product.NameWithoutBrand}</h2>
+
+      <img
+        class="divider"
+        src="${product.Images.PrimaryLarge}"
+        alt="${product.Name}"
+      />
+
+      <p class="product-card__price">$${product.FinalPrice}</p>
+
+      <p class="product__color">${product.Colors[0].ColorName}</p>
+
+      <p class="product__description">
+        ${product.DescriptionHtmlSimple}
+      </p>
+
+      <div class="product-detail__add">
+        <button id="addToCart">Add to Cart</button>
+      </div>
+
+      <ul class="product-detail__list">
+        <li class="product-detail__item">
+          <b>Features</b>
+          <ul>
+            ${product.Features.map(
+              (feature) => `<li>${feature}</li>`
+            ).join('')}
+          </ul>
+        </li>
+      </ul>
+    `;
+  }
+}
